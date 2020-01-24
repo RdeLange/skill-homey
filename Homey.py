9cabd29fcfa79109695c1e36043582b404818f19
rdelange <ronald_de_lange@hotmail.com>
2020-01-24 23:23:01 +0100

    temperature bug fixed

--- Homey.py
+++ Homey.py
@@ -207,7 +207,7 @@ class Homey:
         onnoun = 'on'
         offnoun = 'off'
         thermostatnoun = 'thermostat'
-        thermostatnoun2 = 'termostaat'
+        thermostatnoun2 = 'thermostat'
         lightnoun = 'light'
 
         if self.lang == 'nl-nl':
@@ -217,10 +217,10 @@ class Homey:
             onnoun = 'aan'
             offnoun = 'uit'
             thermostatnoun = 'thermostat'
-            thermostatnoun2 = 'termostaat'
+            thermostatnoun2 = 'thermostaat'
             lightnoun = 'light'
         print(what,temperaturenoun,temperaturenoun2)
-        if what == temperaturenoun or what == temperaturenoun2: what = thermostatnoun2
+        #if what == temperaturenoun or what == temperaturenoun2: what = thermostatnoun2
         print(what,where)
         if where == allnoun:
             data = self.findall(what)
